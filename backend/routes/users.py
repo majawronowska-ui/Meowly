c85c775897964bf611a5fd3f920f4cfa93d4bb22
Maja Wronowska <majawronowska@MacBook-Air-Maja.local>
2026-07-03 17:12:11 +0200

    Remove verification block everywhere

--- backend/routes/users.py
+++ backend/routes/users.py
@@ -3,13 +3,3 @@ from fastapi import APIRouter
 router = APIRouter(prefix="/users", tags=["Users"])
 
 
-@router.get("/")
-def get_users():
-    return [
-        {
-            "id": 1,
-            "name": "Maja",
-            "level": 1,
-            "xp": 0
-        }
-    ]
